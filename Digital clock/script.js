ac7e656660daeca9de9f62975ef4cbf0fa7902d1
Ram-7401 <117374622+Ram-7401@users.noreply.github.com>
2022-11-04 00:21:41 +0530

    Update script.js

--- Digital clock/script.js
+++ Digital clock/script.js
@@ -22,11 +22,14 @@ function update(t) {
  else {
  return t;
  }
-}
-Time();
+ 
 var button = document.getElementById("clickme"),
   count = 0;
 button.onclick = function() {
   count += 1;
   button.innerHTML = "Click me: " + count;
 };
+}
+Time();
+
+
