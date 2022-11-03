50163f97a21cd6bcf253ba9c3cea0ca0ceeb3c56
Ram-7401 <117374622+Ram-7401@users.noreply.github.com>
2022-11-04 00:00:21 +0530

    Add files via upload

---
+++ Digital clock/script.js
@@ -0,0 +1,26 @@
+function Time() {
+ var date = new Date();
+ var hour = date.getHours();
+ var minute = date.getMinutes();
+ var second = date.getSeconds();
+ var period = "";
+ if (hour >= 12) {
+ period = "PM";
+ } else {
+ period = "AM";
+ }
+ hour = update(hour);
+ minute = update(minute);
+ second = update(second);
+ document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
+ setTimeout(Time, 1000);
+}
+function update(t) {
+ if (t < 10) {
+ return "0" + t;
+ }
+ else {
+ return t;
+ }
+}
+Time();
